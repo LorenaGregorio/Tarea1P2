c1f00d4676f6408eb8f85a3770d8854c842ad05f
Lorena Gregorio Hernandez <lorenaghernandez@hotmail.com>
2019-01-21 17:25:24 -0600

    SE AGREGARON TODOS LOS BOTONES SE PROGRAMO EL BOTON C SE AGREGARON Y SE INICIALIZARON LAS VARIABLES

--- Tarea1CalculadoraUDEO/Form1.cs
+++ Tarea1CalculadoraUDEO/Form1.cs
@@ -16,5 +16,44 @@ namespace Tarea1CalculadoraUDEO
         {
             InitializeComponent();
         }
+
+        // SE DECLARARON LAS VARIABLES DE USO PARA LAS OPERACIONES
+        // SE DECLARARON PUBLICAS PARA QUE SE PUEDAN USAR EN TODO EL PROGRAMA
+        // SE INICIALIZO EL VALOR DE LA VARIABLES SEGUN SU TIPO
+        public string valorInicial = "";
+        public string valorFinal = "";
+        public string signoActual = "";
+        public int contadorCalculos = 0;
+
+        // SE DECLARO UN METODO PARA REINICIAR LOS VALORES DE LA CALCULADORA
+
+        private void ReiniciarValores()
+        {
+            signoActual = "";
+            valorInicial = "";
+            valorFinal = "";
+            contadorCalculos = 0;
+        }
+
+        // 
+        public void AgregarValorInicial(string valor)
+        {
+            valorInicial += valor;
+            PANTALLA.Text = valorInicial.ToString();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            ReiniciarValores();
+            PANTALLA.Text = "";
+
+        }
     }
 }
